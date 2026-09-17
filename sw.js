b79da836a98e4db0073b17eee8bb46cd4417fd9f
alanyen-git <jjaster2011@gmail.com>
2026-09-18 02:38:44 +0800

    chore: refresh PWA cache for naming protection

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX="qunlu-pwa-";
-const CACHE_NAME=CACHE_PREFIX+"v12";
+const CACHE_NAME=CACHE_PREFIX+"v13";
 const CORE=[
   "./",
   "./index.html",
@@ -23,6 +23,7 @@ const CORE=[
   "./src/player-experience-guidance-v1.js",
   "./src/world-naming-v1.js",
   "./src/world-naming-v2.js",
+  "./src/world-naming-protection-v1.js",
   "./src/release-version-sync-v1.js",
   "./src/pwa.js",
   "./icons/icon-192.png",
