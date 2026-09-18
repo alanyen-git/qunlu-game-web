41515ef6452dd533af5186371b0a5befd560a72a
alanyen-git <jjaster2011@gmail.com>
2026-09-18 22:25:17 +0800

    release: cache affiliation contribution module

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX="qunlu-pwa-";
-const CACHE_NAME=CACHE_PREFIX+"v33";
+const CACHE_NAME=CACHE_PREFIX+"v34";
 const CORE=[
   "./",
   "./index.html",
@@ -16,6 +16,7 @@ const CORE=[
   "./src/market-economy-v2.js",
   "./src/world-autonomy-v1.js",
   "./src/runtime-patches.js",
+  "./src/affiliation-contribution-v1.js",
   "./src/world-autonomy-v2.js",
   "./src/npc-depth-v1.js",
   "./src/npc-depth-v2.js",
