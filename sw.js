5e34ed0f33fa5b2b6b597a5401e9e28c3ef2afb7
alanyen-git <jjaster2011@gmail.com>
2026-09-19 16:36:55 +0800

    release: cache skill naming reference

--- sw.js
+++ sw.js
@@ -32,6 +32,7 @@ const CORE=[
   "./src/class-skill-optimization-v1.js",
   "./src/class-skill-passive-compat-v1.js",
   "./src/class-naming-reference-v1.js",
+  "./src/skill-naming-reference-v1.js",
   "./src/system-integrity-v2.js",
   "./src/affiliation-integrity-v1.js",
   "./src/player-experience-guidance-v1.js",
